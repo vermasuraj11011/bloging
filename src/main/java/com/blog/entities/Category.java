9fd25c3163bb9c20819f626efc04e350f435c6cc
vermasuraj11011 <vermasuraj008@gmail.com>
2023-06-30 12:28:44 +0530

    adding entities

---
+++ src/main/java/com/blog/entities/Category.java
@@ -0,0 +1,30 @@
+package com.blog.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title", nullable = false, length = 100)
+    private String categoryTitle;
+
+    @Column(name = "description")
+    private String categoryDescription;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Post> posts = new ArrayList<>();
+}
---
+++ src/main/java/com/blog/entities/Post.java
@@ -0,0 +1,40 @@
+package com.blog.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "post")
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "title", nullable = false, length = 100)
+    private String postTitle;
+
+    @Column(name = "content", length = 10000, nullable = false)
+    private String postContent;
+
+    private String imageName;
+
+    @Column(name = "added_date")
+    private Date addedDate;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
