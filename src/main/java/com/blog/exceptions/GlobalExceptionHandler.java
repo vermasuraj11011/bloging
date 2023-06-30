a66c4e8184be6820b18ee5871dd7f7201b78477f
vermasuraj11011 <vermasuraj008@gmail.com>
2023-06-30 12:42:51 +0530

    adding exception

---
+++ src/main/java/com/blog/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.blog.exceptions;
+
+import com.blog.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex){
+        String message = ex.getMessage();
+        ApiResponse apiResponse = new ApiResponse(message,false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex){
+        Map<String,String> methodNotValidMap = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) ->{
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            methodNotValidMap.put(fieldName,message);
+        });
+        return new ResponseEntity<>(methodNotValidMap, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ApiResponse> apiExceptionHandler(ApiException ex){
+        String message = ex.getMessage();
+        ApiResponse apiResponse = new ApiResponse(message,false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/com/blog/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,27 @@
+package com.blog.exceptions;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResourceNotFoundException extends RuntimeException {
+    String resourceName;
+    String fieldName;
+    long fieldValue;
+    String stringFieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue) {
+        super(String.format("%s not found with %s : %s", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public ResourceNotFoundException(String resourceName, String fieldName, String stringFieldValue) {
+        super(String.format("%s not found with %s : %s", resourceName, fieldName, stringFieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.stringFieldValue = stringFieldValue;
+    }
+}
