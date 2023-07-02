c56ed1475ad6ac8bfdc0fabd14f45bd0286a5b01
vermasuraj11011 <vermasuraj008@gmail.com>
2023-07-02 14:14:57 +0530

    removing deprecated WebSecurityConfigurerAdapter

--- src/main/java/com/blog/config/SecurityConf.java
+++ src/main/java/com/blog/config/SecurityConf.java
@@ -7,13 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -21,7 +24,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebSecurity
 @EnableWebMvc
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConf extends WebSecurityConfigurerAdapter {
+public class SecurityConf {
+
+//
+//      WebSecurityConfigurerAdapter is  deprecated
+//      extends WebSecurityConfigurerAdapter {
 
     public static final String[] PUBLIC_URLS = {
             "/api/v1/auth/**",
@@ -44,19 +51,10 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
     @Autowired
     private JWTAuthenticationFilter jwtAuthenticationFilter;
 
-    /**
-     * basic authentication
-     * http
-     * .csrf().disable()
-     * .authorizeHttpRequests()
-     * .anyRequest()
-     * .authenticated()
-     * .and()
-     * .httpBasic();
-     */
+    //    new method added
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeHttpRequests()
@@ -70,24 +68,76 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+        http.authenticationProvider(daoAuthenticationProvider());
+
         http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        DefaultSecurityFilterChain defaultSecurityFilterChain = http.build();
+        return defaultSecurityFilterChain;
     }
 
+    /**
+     * basic authentication
+     * http
+     * .csrf().disable()
+     * .authorizeHttpRequests()
+     * .anyRequest()
+     * .authenticated()
+     * .and()
+     * .httpBasic();
+     */
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(this.customUserDetailService).passwordEncoder(passwordEncoder());
+    /**
+     * This is commented as WebSecurityConfigurerAdapter is deprecated
+     *
+     * @Override protected void configure(HttpSecurity http) throws Exception {
+     * http
+     * .csrf().disable()
+     * .authorizeHttpRequests()
+     * .antMatchers(PUBLIC_URLS).permitAll()
+     * .anyRequest()
+     * .authenticated()
+     * .and()
+     * .exceptionHandling()
+     * .authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
+     * .and()
+     * .sessionManagement()
+     * .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+     * http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+     * }
+     *
+     *
+     *8@Override
+     *protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+     *auth.userDetailsService(this.customUserDetailService).passwordEncoder(passwordEncoder());
+     *}
+     *
+     * @Override protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+     * auth.userDetailsService(this.customUserDetailService).passwordEncoder(passwordEncoder());
+     * }
+     * @Bean
+     * @Override public AuthenticationManager authenticationManagerBean() throws Exception {
+     * return super.authenticationManagerBean();
+     * }
+     */
+
+
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(this.customUserDetailService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
 }
